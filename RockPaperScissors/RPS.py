18e512826b4d4f1131a1e9badd573c1c54752031
Matthew <matthew.hilbrink@outlook.com>
2017-01-11 18:22:44 +0100

    Not tested

--- RockPaperScissors/RPS.py
+++ RockPaperScissors/RPS.py
@@ -1,1 +1,34 @@
-print("Hello World!")
+from random import randint
+
+actions = ["Rock", "Paper", "Scissors"]
+
+computer = t[randint(0, 2)]
+
+player = False
+
+while Player == false
+    player = input("Rock, Paper, Scissors")
+    if player == computer
+        print
+        "Draw"
+        elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+        else:
+            print("You win!"
+            player, "covers", computer)
+        elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose.", computer, "smashes", player)
+        else:
+            print("You win!", player, "cut", computer)
+    else:
+        print("Rock, Paper, Scissors")
+
+    player = False
+    computer = t[randint(0, 2)]
