48d66fc4e735a595bfc7aa9187ded7b9bb58362e
Matthew <matthew.hilbrink@outlook.com>
2017-01-12 21:19:53 +0100

    Updates

--- Utilities.py
+++ Utilities.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from random import randint
 import random
 import sys
 import time
@@ -13,31 +14,31 @@ for x in range(100):
     if x == 99:
         print("[100] loaded succesfully ")
 
-test = input("Enter password: ")
-while test != "Hello":
+passwrd = input("Enter password: ")
+while passwrd != "Hello":
     print("Invalid Password!")
-    sys.exit("Acces denied")
+    sys.exit("Access denied")
 
 print(" - Access granted - ")
-time = datetime.now()
+t = datetime.now()
 
-print('Year = %s' % time.year)
-print('Month = %s' % time.month)
-print('Day = %s' % time.day)
-print('Hour = %s' % time.hour)
-print('Minutes = %s' % time.minute)
-print('Microsecond = %s' % time.microsecond)
+print('Year = %s' % t.year)
+print('Month = %s' % t.month)
+print('Day = %s' % t.day)
+print('Hour = %s' % t.hour)
+print('Minutes = %s' % t.minute)
+print('Microsecond = %s' % t.microsecond)
 
-print("Time = %s:%s:%s" % (time.hour, time.minute, time.second))
-print("Date =  %s/%s/%s" % (time.day, time.month, time.year))
+print("Time = %s:%s:%s" % (t.hour, t.minute, t.second))
+print("Date =  %s/%s/%s" % (t.day, t.month, t.year))
 
-commands = input(">>>: ")
+#commands = input(">>>: ")
 while commands != "clr" + "test" + "calc":
-    commands = input(">>>: ")
+    #commands = input(">>>: ")
     if commands == "clr":
         print("\n" * 100)
     elif commands == "help":
-        print("Commands:\ntest {performs a test}\ncalc {A calculator}\nclr {clears console}\n - More commands soon -")
+        print("Commands:\ntest {performs a test}\ncalc {A calculator}\nclr {clears console}\nRPS {Rock Paper Scissors game}\n - More commands soon -")
     elif commands == "test":
         print("Hello this is a test.")
     elif commands == "calc":
@@ -52,3 +53,42 @@ while commands != "clr" + "test" + "calc":
             print(num1 * num2)
         if oper == "%":
             print(num1 % num2)
+    elif commands == "RPS":
+        actions = ["Rock", "Paper", "Scissors"]
+        computer = actions[randint(0, 2)]
+        player = False
+        while not player:
+            time.sleep(2.5)
+            print("\n" * 40)
+            print("Loading.")
+            time.sleep(0.6)
+            print("\n" * 40)
+            print("Loading..")
+            time.sleep(1.2)
+            print("\n" * 40)
+            print("Loading...")
+            time.sleep(0.5)
+            print("\n" * 40)
+            player = input("Rock, Paper, Scissor\nChoice: ")
+            if player == computer:
+                print("Draw!")
+            elif player == "Rock":
+                if computer == "Paper":
+                    print("You lose!", computer, "covers", player)
+                else:
+                    print("You win!", player, "smashes", computer)
+            elif player == "Paper":
+                if computer == "Scissors":
+                    print("You lose!", computer, "cut", player)
+                else:
+                    print("You win!", player, "covers", computer)
+            elif player == "Scissors":
+                if computer == "Rock":
+                    print("You lose!", computer, "smashes", player)
+                else:
+                    print("You win!", player, "cut", computer)
+            else:
+                print("Rock, Paper or Scissors")
+
+            player = False
+            computer = actions[randint(0, 2)]
