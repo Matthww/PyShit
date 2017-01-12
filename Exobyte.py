371678f48a536fc8e78a50fda5dfc72a604fef58
Matthew <matthew.hilbrink@outlook.com>
2017-01-12 21:39:15 +0100

    Add RPS to Exobyte
    And delete Utilities

--- Exobyte.py
+++ Exobyte.py
@@ -37,7 +37,8 @@ while commands != "clr" + "test" + "calc":
     if commands == "clr":
         print("\n" * 100)
     elif commands == "help":
-        print("Commands:\ntest {performs a test}\ncalc {A calculator}\nclr {clears console}\n - More commands soon -")
+        print("Commands:\ntest {performs a test}\ncalc {A calculator}\nclr {clears console}\nRPS {Rock Paper Scissors game}")
+        print("- More commands soon -")
     elif commands == "test":
         print("Hello this is a test.")
     elif commands == "calc":
@@ -52,3 +53,42 @@ while commands != "clr" + "test" + "calc":
             print(num1 * num2)
         if oper == "%":
             print(num1 % num2)
+    elif commands == "RPS":
+        actions = ["Rock", "Paper", "Scissors"]
+        computer = actions[randint(0, 2)]
+        player = False
+        while not player:
+            time.sleep(2.5)
+            print("\n" * 40)
+            print("Loading.")
+            time.sleep(0.6)
+            print("\n" * 40)
+            print("Loading..")
+            time.sleep(1.2)
+            print("\n" * 40)
+            print("Loading...")
+            time.sleep(0.5)
+            print("\n" * 40)
+            player = input("Rock, Paper, Scissor\nChoice: ")
+            if player == computer:
+                print("Draw!")
+            elif player == "Rock":
+                if computer == "Paper":
+                    print("You lose!", computer, "covers", player)
+                else:
+                    print("You win!", player, "smashes", computer)
+            elif player == "Paper":
+                if computer == "Scissors":
+                    print("You lose!", computer, "cut", player)
+                else:
+                    print("You win!", player, "covers", computer)
+            elif player == "Scissors":
+                if computer == "Rock":
+                    print("You lose!", computer, "smashes", player)
+                else:
+                    print("You win!", player, "cut", computer)
+            else:
+                print("Rock, Paper or Scissors")
+
+            player = False
+            computer = actions[randint(0, 2)]
