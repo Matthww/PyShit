9d18e6e33aaff62490c01f504de144ad15aa6bd8
strNophix <nickpoelson@gmail.com>
2017-01-13 19:38:04 +0100

    Added some coloring

--- Windows.py
+++ Windows.py
@@ -1,4 +1,17 @@
-import tkinter
-top = tkinter.Tk()
-top.mainloop()
-top.
+from termcolor import colored, cprint
+
+text1 = colored(text="Commands: ", color='red')
+print(text1)
+
+print_grey_on_white = lambda x: cprint(x, 'grey', 'on_white')
+print_grey_on_white('Hello, World!')
+
+print_whiteblue = lambda x: cprint(x, 'white', 'on_blue')
+print_whiteblue('Hello, World!')
+
+print1 = lambda x: cprint(x, 'grey', 'on_green')
+print1('Hello, World!')
+
+cmdcol = colored('>>>: ', attrs=['bold'])
+print(cmdcol)
+sade = input("press key to continue...")
