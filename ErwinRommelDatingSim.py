8b6a66bb0c7bc4e16753a9cb7e4cfc5e03e9f328
strNophix <nickpoelson@gmail.com>
2017-01-13 11:34:15 +0100

    Push push push

---
+++ ErwinRommelDatingSim.py
@@ -0,0 +1,44 @@
+import sys
+
+name = input("What is your name? ")
+print("Hello " + name)
+print("Nice to meet you and oohh nice shoes, wnanna bang lmao")
+bang = input("You want to bang? (yes or no) ")
+if bang == "yes":
+    print("Kyaaaaaaaaaaaaa")
+else:
+    if bang == "no":
+        print("Get the fuck out of my way")
+        print("You goofed with the wrong person")
+        sys.exit(0)
+    else:
+        print("Hoe bye")
+        sys.exit("Go away u piece of shit")
+
+print("*Two hours later* "
+      "???: Oeh that was nice")
+print(name + ": Yes, whats your name?")
+print("???: My name is Erwin Rommel, babe")
+haha = input("Erwin Rommel: How about you? ")
+if haha != name:
+    print("Your drivers license says something else, u liar *walks away*")
+    sys.exit(0)
+else:
+    print("Erwin Rommel: Hmmmmm")
+print(name + ": Hey, thats pretty good, but I got to go, ill ttyl *gives phonenumber*")
+phone = int(input("Enter phone number here "))
+print("Erwin Rommel: VERDAMPT JA")
+print(name + ": Bye")
+
+
+SJW = input("ARE YOU A SJW? ")
+Freunde = input("Habt du freuden? ")
+# More storyline soon
+f = open('dox.txt', 'w')
+f.write('D0X:' + '\n')
+f.write('name = ' + repr(name) + '\n')
+f.write('phone = ' + repr(phone) + '\n')
+f.write('sjw = ' + repr(SJW) + '\n')
+f.write('freunden = ' + repr(Freunde) + "\n")
+f.close()
+print("Ty for the dox Kappa")
---
+++ test.py
@@ -0,0 +1,96 @@
+from random import randint
+
+commands = input(">>>: ")
+
+def rps_command_interface():
+    if commands == "snow":
+        actions = ["*", " ", " ", "*", " "]
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer, end="")
+        computer = actions[randint(0, 3)]
+        print(computer)
+rps_command_interface()
